0ab4a3e7949d039f0550ffc18826820946f868dc
Relsell <relsellglobal@gmail.com>
2018-07-08 19:36:38 +0530

    added server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 9003;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
